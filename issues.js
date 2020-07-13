1b41aca321f3ce021ab2ee8ce6ed571947861d89
aldav99 <aldav99@yandex.ru>
2020-07-13 08:38:56 +0300

    Try # 2

--- issues.js
+++ issues.js
@@ -1,49 +1,96 @@
-// let url = "https://api.github.com/repos/octocat/Hello-World/issues/1347";
+let elemBtn = document.getElementById('btn');
 
-let elem = document.getElementById('btn');
-
-elem.addEventListener('click', fetchIssue);
-
-// let url = "https://api.github.com/repos/octocat/Hello-World/issues";
-
-
-// let url = "https://github.com/repos/aldav99/jsItog/issues/1";
-
-// https://github.com/facebook/react/issues
+elemBtn.addEventListener('click', fetchIssue);
+elemBtn.addEventListener('click', launch);
 
 async function fetchAsync() {
     let inptUrl = document.getElementById('url');
     let url = inptUrl.value;
-    // await response of fetch call
-    // let response = await fetch(url, { filter: 'subscribed' });
+
+    // DEBUG
+    // let url = "https://api.github.com/repos/octocat/Hello-World/issues";
+    // let url = "https://api.github.com/rep";
+    // DEBUG
     let response = await fetch(url);
-    // let response = await fetch(url, {
-    //     headers: new Headers({
-    //         'Access-Control-Allow-Origin': '*'
-    //     })
-    // });
-    // only proceed once promise is resolved
-    let data = await response.json();
-    // only proceed once second promise is resolved
+    if (!response.ok) {
+        throw new Error(`Status: ${response.status} (${response.statusText})`);
+    }
+    const reader = response.body.getReader();
+    const contentLength = +response.headers.get('Content-Length');
+
+    let receivedLength = 0;
+    let chunks = [];
+
+    while (true) {
+        const { done, value } = await reader.read();
+
+        if (done) {
+            break;
+        }
+
+        chunks.push(value);
+        receivedLength += value.length;
+
+        console.log(`Получено ${receivedLength} из ${contentLength}`)
+    }
+
+    let chunksAll = new Uint8Array(receivedLength); // (4.1)
+    let position = 0;
+    for (let chunk of chunks) {
+        chunksAll.set(chunk, position); // (4.2)
+        position += chunk.length;
+    }
+    let result = new TextDecoder("utf-8").decode(chunksAll);
+    let data = JSON.parse(result);
     return data;
 }
 
-// trigger async function
-// log response or catch error of fetch promise
 function fetchIssue() {
     fetchAsync()
         .then(data => data.map(function (element) {
-            // console.table(element.number,
-            //     element.created_at, element.title, element.body)
             return addResultData(element);
         }))
-        .catch(reason => console.log(reason.message))
+        .catch(reason => errorMessage(reason))
+}
+
+function launch() {
+    let btn = document.getElementById('btn');
+    let myProgress = document.createElement('div');
+    myProgress.setAttribute("id", "myProgress");
+
+    let myBar = document.createElement('div');
+    myBar.setAttribute("id", "myBar");
+    myProgress.appendChild(myBar);
+
+    btn.after(myProgress);
+
+    let i = 0;
+    function move() {
+        if (i == 0) {
+            i = 1;
+            let width = 1;
+            let id = setInterval(frame, 10);
+            function frame() {
+                if (width >= 100) {
+                    clearInterval(id);
+                    i = 0;
+                } else {
+                    width++;
+                    myBar.style.width = width + "%";
+                }
+            }
+        }
+    }
+    return move();
 }
 
 function addResultData(data) {
     let resultOutput = document.getElementById('result');
     let divCore = document.createElement('div');
-    divCore.classList.add("core-border", "parent");
+    let table1 = document.createElement('table');
+    table1.classList.add("blueTable");
+    table1.setAttribute("style", "width:100%;text-align:center;");
+
     let tr1 = document.createElement('tr');
 
     let tdNumber = document.createElement('td');
@@ -53,30 +100,78 @@ function addResultData(data) {
 
     let tdCreatedAt = document.createElement('td');
     let textCreatedAt = document.createTextNode(data.created_at.trim());
-    tdCreatedAt.classList.add("data-before");
     tdCreatedAt.appendChild(textCreatedAt);
 
     tr1.appendChild(tdNumber);
     tr1.appendChild(tdCreatedAt);
 
-    console.log(tr1.children)
+    table1.appendChild(tr1);
+    // -----------------------------------------------
+    let table2 = document.createElement('table');
+    table2.setAttribute("style", "width:100%;text-align:center;");
+    table2.classList.add("purpurTable");
 
     let tr2 = document.createElement('tr');
+    let tdTitle = document.createElement('td');
 
-    // let divTitle = document.createElement('div');
     let textTitle = document.createTextNode(data.title);
-    tr2.appendChild(textTitle);
+    tdTitle.appendChild(textTitle);
+    tr2.appendChild(tdTitle);
+    table2.appendChild(tr2);
+
+    // -----------------------------------------------
+
+    let table3 = document.createElement('table');
+    table3.setAttribute("style", "width:100%;text-align:center;");
+    table3.classList.add("greenTable");
 
     let tr3 = document.createElement('tr');
+    let tdBody = document.createElement('td');
+    let textString = data.body;
+    let textBody;
+    if (textString) {
+        textBody = document.createTextNode(textString.substring(0, 100));
+    } else {
+        textBody = document.createTextNode("Empty String");
+    }
 
-    // let divBody = document.createElement('div');
-    let textBody = document.createTextNode(data.body);
-    tr3.appendChild(textBody);
+    tdBody.appendChild(textBody);
+    tr3.appendChild(tdBody);
+    table3.appendChild(tr3);
 
-    divCore.appendChild(tr1);
-    divCore.appendChild(tr2);
-    divCore.appendChild(tr3);
-    // divCore.appendChild(divBody);
+    // -----------------------------------------------
+
+    divCore.appendChild(table1);
+    divCore.appendChild(table2);
+    divCore.appendChild(table3);
 
     resultOutput.appendChild(divCore);
+}
+
+function errorMessage(reason) {
+    let resultOutput = document.getElementById('result');
+    let divError = document.createElement('div');
+    divError.setAttribute("id", "Error");
+    let textError = document.createTextNode(reason);
+    divError.appendChild(textError);
+    resultOutput.appendChild(divError);
+
+    let btnDelete = document.createElement('button');
+    btnDelete.innerHTML = "DELETE";
+    btnDelete.setAttribute("id", "btnError");
+
+    divError.after(btnDelete);
+
+    btnDelete.addEventListener('click', messageDelete);
+}
+
+function messageDelete() {
+    let myProgress = document.getElementById('myProgress');
+    myProgress.remove();
+
+    let divError = document.getElementById("Error");
+    divError.remove();
+
+    let btnError = document.getElementById("btnError");
+    btnError.remove();
 }
